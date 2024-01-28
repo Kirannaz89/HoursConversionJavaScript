529b2631c169e095560756ee3f35eab6ab47f200
Kirannaz89 <Nazkiran1989.gmail.com>
2024-01-28 21:36:09 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function howManyMinutes() {
+
+	var hours = document.getElementById("hours").value;
+	var result = hours * 60;
+	document.getElementById("minutes").value = result;
+
+
+	
+}
+
+
+function howManySeconds() {
+
+	var hours = document.getElementById("Hours").value;
+	var result = hours * 60 * 60;
+	document.getElementById("seconds").value = result;
+
+
+	
+}
+
